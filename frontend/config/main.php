a3b19a931f0242b56059ec553c68180b6def545d
João Marques <joao@jjmf.com>
2016-05-21 17:21:40 -0300

    Frontend

---
+++ frontend/config/main.php
@@ -0,0 +1,49 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            //'hostname' => 'localhost',
+            //'hostname' => '10.70.20.188',
+            'hostname' => '172.17.0.4',
+            'port' => 6379,
+            'database' => 0,
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+    ],
+    'params' => $params,
+];
---
+++ frontend/views/site/index.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Project: VanHackathon May 2016
+ * User: joao
+ * Email: joao@jjmf.com
+ * Date: 21/05/16
+ * Time: 15:57
+ */
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+
+$this->title = 'Chat';
+
+$js = <<<JS
+$('#chat-form').submit(function() {
+
+     var form = $(this);
+
+     $.ajax({
+          url: form.attr('action'),
+          type: 'post',
+          data: form.serialize(),
+          success: function (response) {
+               $("#message-field").val("");
+          }
+     });
+
+     return false;
+});
+JS;
+$this->registerJs($js, \yii\web\View::POS_READY)
+?>
+<div class="site-index">
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="well col-lg-8 col-lg-offset-2">
+
+                <?= Html::beginForm(['/site/index'], 'POST', [
+                    'id' => 'chat-form'
+                ]) ?>
+
+                <div class="row">
+                    <div class="col-xs-3">
+                        <div class="form-group">
+                            <?= Html::textInput('name', null, [
+                                'class' => 'form-control',
+                                'placeholder' => 'Name'
+                            ]) ?>
+                        </div>
+                    </div>
+                    <div class="col-xs-7">
+                        <div class="form-group">
+                            <?= Html::textInput('message', null, [
+                                'id' => 'message-field',
+                                'class' => 'form-control',
+                                'placeholder' => 'Message'
+                            ]) ?>
+                        </div>
+                    </div>
+                    <div class="col-xs-2">
+                        <div class="form-group">
+                            <?= Html::submitButton('Send', [
+                                'class' => 'btn btn-block btn-success'
+                            ]) ?>
+                        </div>
+                    </div>
+                </div>
+
+                <?= Html::endForm() ?>
+
+                <div id="notifications" ></div>
+            </div>
+        </div>
+
+    </div>
+</div>
